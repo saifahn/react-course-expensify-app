b49f10305991775db3eb385216cb22798e0914e7
Sean Li <li.saifahn@gmail.com>
2018-06-05 18:03:11 +0100

    [REM] Remove bundle.js from being tracked by git [REF] Separate out webpack config for production and dev

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+// const path = require('path');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/,
+    }, {
+      test: /\.s?css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        // 'style-loader',
+        'css-loader',
+        'sass-loader',
+      ],
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'styles.css',
+    }),
+  ],
+  // plugins: [
+  //   new CleanWebpackPlugin
+  // ]
+};
