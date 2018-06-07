59268630ce61a199b28209e68c2f058134e7a090
Sean Li <li.saifahn@gmail.com>
2018-06-07 08:09:05 +0100

    [REF] Update ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
-import getVisibleExpenses from '../selectors/expenses';
-import getExpensesTotal from '../selectors/expenses-total';
+import selectVisibleExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
 
 
-export const ExpensesSummary = props => (
-  <div>
-    {props.expenseCount > 0
-      &&
-      <p>Viewing {props.expenseCount} expense{props.expenseCount > 1
-        && 's'}, totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}
-      </p>}
-  </div>
-);
+// export const ExpensesSummary = props => (
+//   <div>
+//     {props.expenseCount > 0
+//       &&
+//       <p>Viewing {props.expenseCount} expense{props.expenseCount > 1
+//         && 's'}, totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}
+//       </p>}
+//   </div>
+// );
+
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div>
+      <h2>Viewing {expenseCount} {expenseWord}, totalling {formattedExpensesTotal}</h2>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-  const expenses = getVisibleExpenses(state.expenses, state.filters);
-  const expensesTotal = getExpensesTotal(expenses);
+  const expenses = selectVisibleExpenses(state.expenses, state.filters);
+  const expensesTotal = selectExpensesTotal(expenses);
   return {
     expenseCount: expenses.length,
     expensesTotal,
